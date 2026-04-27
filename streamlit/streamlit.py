4c32fc86a78ac76132f57107e692f04fd44c2c1e
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-04-27 10:10:47 +0200

    feat:getting alligned with the main branch

---
+++ streamlit/streamlit.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+import os 
+from datetime import datetime
+
+# This will be for the start page and includes the title emoji and layout. Note that we are planing to add the logo later on.
+st.set_page_config(
+    page_title= "CoinCompass",
+    page_icon= "🧭",
+    layout= "wide",
+)
+#--
+
+
+
+
+
+
+    
+  
