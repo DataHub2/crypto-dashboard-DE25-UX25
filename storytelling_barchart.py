0753552a5bf7ca9ed0b4fdbbab2e5d28f3cd0735
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-05-01 00:05:39 +0200

    feat: added the path to the gold data set and isolated it so that it finds the data that we are going to be wotking on. Now we should have 2 different data sets

--- storytelling_barchart.py
+++ storytelling_barchart.py
@@ -4,5 +4,12 @@ import glob
 import numpy as np
 
 # ingesting the cleaned data
-df = pd.read_csv("data/interim/master_cleaned.csv")
+df = pd.read_csv("data/interim/master_cleaned.csv") 
 df["date"] = pd.to_datetime(df["date"])
+
+# Isolate the data from gold
+gold_path = glob.glob("data/interim/Download Data*.csv")[0]
+gold = pd.read_csv(gold_path)
+gold.columns = gold.columns.str.strip()
+gold["Close"] = gold["Close"].astype(str).str.replace(',', '', regex=False).astype(float)
+
