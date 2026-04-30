a2e00483a5589fa71fcc968fa667ed5a2a684b47
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-04-30 23:53:38 +0200

    feat: added code that directs to my data set and also ingests the right data that we are going to be using in this script

--- storytelling_barchart.py
+++ storytelling_barchart.py
@@ -2,3 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 import numpy as np
+
+# ingesting the cleaned data
+df = pd.read_csv("data/interim/master_cleaned.csv")
+df["date"] = pd.to_datetime(df["date"])
