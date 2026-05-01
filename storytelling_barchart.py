3e688dc8029156dda614381d59f2121a3c1c6c80
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-05-01 10:56:07 +0200

    feat: i added the plot horizontab bars and then removed the spines so that i maximaze the data-ink ratio

--- storytelling_barchart.py
+++ storytelling_barchart.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import glob
 import numpy as np
 
+
+"""
+This script is for the barchart for storytelling of the volitility for the choosen assets.
+The goal with this is to make the barchart is to follow the the principels of data storytelling so that we end up with a a result that mirrors my goal with. the visuals.
+
+"""
+
 # ingesting the cleaned data
 df = pd.read_csv("data/interim/master_cleaned.csv") 
 df["date"] = pd.to_datetime(df["date"])
@@ -64,3 +71,18 @@ TEXT_COLOR = '#333333'
 fig, ax = plt.subplots(figsize=(10, 6.5))
 fig.patch.set_facecolor(BG_COLOR)
 ax.set_facecolor(BG_COLOR)
+
+# Identify stable assets and highlighting them
+highlight_assets = ['Bitcoin', 'Gold']
+colors = [BAR_HIGHLIGHT if asset in highlight_assets else BAR_GRAY for asset in df_plot['Asset']]
+
+#  bar plotting on the horizontal axis, and we are using the colors list to set the color of each bar based on whether it's a stable asset or not.
+bars = ax.barh(df_plot['Asset'], df_plot['Volatility'], color=colors, height=0.6, zorder=3)
+
+#structural framing to maximize Data-Ink Ratio
+for spine in ax.spines.values():
+    spine.set_visible(False) # Remove all spines for a cleaner look on the visuals. 
+
+
+
+
