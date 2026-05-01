e752aa02050d3eae96e242c22e59ab81f6be0e94
ZakariaAbdullahi-Portfolio <zakariaallmaninfo@gmail.com>
2026-05-01 02:18:15 +0200

    feat: added the calculation on volitility (risk) for both gold and crypto currencies

--- storytelling_barchart.py
+++ storytelling_barchart.py
@@ -13,3 +13,27 @@ gold = pd.read_csv(gold_path)
 gold.columns = gold.columns.str.strip()
 gold["Close"] = gold["Close"].astype(str).str.replace(',', '', regex=False).astype(float)
 
+# Because i dont know exactly what the column name variations are, i am going to be working with a if and else statement to make sure that the date column is properly converted.
+if "Date" in gold.columns:
+    gold["date"] = pd.to_datetime(gold["Date"])
+else:
+    gold["date"] = pd.to_datetime(gold["date"])
+
+# -- Note that LLM helped me with getting the right syntax for calculating the the daily percentage (line 30-34)
+# On this chapter i am going to get the right calculation.
+results = []
+
+# process speculative and "standard" crypto assets
+for coin in df["coin_id"].unique(): # Loops through each unique coin_id
+    cdf = df[df["coin_id"] == coin].sort_values("date").copy()
+    # Calculate daily percentage change
+    cdf["daily_return"] = cdf["price"].pct_change() * 100
+    # Calculate standard deviation of these returns
+    volatility = cdf["daily_return"].std()
+    results.append({"Asset": coin, "Volatility": volatility})
+
+# Process traditional asset (Gold)
+gdf = gold.sort_values("date").copy()
+gdf["daily_return"] = gdf["Close"].pct_change() * 100
+g_volatility = gdf["daily_return"].std()
+results.append({"Asset": "gold", "Volatility": g_volatility})
